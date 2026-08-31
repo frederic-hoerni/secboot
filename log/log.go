5e32f55fa06c115e552068fab76d3b885e35e17c
Frederic Hoerni <frederic.hoerni@canonical.com>
2026-08-31 11:05:48 +0200

    add package log

--- log/log.go
+++ log/log.go
@@ -5,25 +5,41 @@ import (
 	"os"
 )
 
-var active bool = false
+// Messages with a log level <= this value shall be logged
+// -1 means no logging
+var logLevelPolicy = -1
 
-func Activate() {
-	active = true
+// Log levels, partly mapped on syslog log levels
+const (
+	LogLevelErr  = 3 // error conditions
+	LogLevelWarn = 4 // warning conditions
+	LogLevelInfo = 6 // informational message
+	LogLevelDebug = 7 //
+)
+
+func SetLogLevel(level int) {
+	logLevelPolicy = level
+}
+
+func Log(level int, format string, v ...any) {
+	if level <= logLevelPolicy {
+		fmt.Fprintf(os.Stderr, format, v...)
+		fmt.Fprintf(os.Stderr, "\n")
+	}
+}
+
+func Debug(format string, v ...any) {
+	Log(LogLevelDebug, format, v...)
 }
 
 func Info(format string, v ...any) {
-	if !active {
-		return
-	}
-	fmt.Printf(format, v...)
-	fmt.Printf("\n")
+	Log(LogLevelInfo, format, v...)
+}
+
+func Warning(format string, v ...any) {
+	Log(LogLevelWarn, format, v...)
 }
 
 func Error(format string, v ...any) {
-	if !active {
-		return
-	}
-	fmt.Fprintf(os.Stderr, "error: ")
-	fmt.Fprintf(os.Stderr, format, v...)
-	fmt.Fprintf(os.Stderr, "\n")
+	Log(LogLevelErr, format, v...)
 }
