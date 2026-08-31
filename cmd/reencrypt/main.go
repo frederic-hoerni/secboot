2f9f914e5076e7d8627cf672c38a3abd3c4fe8a0
Frederic Hoerni <frederic.hoerni@canonical.com>
2026-08-31 12:03:27 +0200

    cmd/reencrypt: fix

--- cmd/reencrypt/main.go
+++ cmd/reencrypt/main.go
@@ -2,26 +2,41 @@ package main
 
 import (
 	"context"
+	"encoding/hex"
 	"fmt"
+	"strings"
 	"github.com/snapcore/secboot"
+	"github.com/snapcore/secboot/log"
 	_ "github.com/snapcore/secboot/luks2" // This gets the LUKS2 backend initialized
 	"os"
 )
 
-const Usage = `usage: reencrypt [options] ACTIVE-NAME
+const Usage = `
+usage: 1. reencrypt              ACTIVE-NAME KEYSLOT-NAME:UNLOCK-KEY-HEX ...
+       2. reencrypt --status     ACTIVE-NAME
+       3. reencrypt --initialize ACTIVE-NAME KEYSLOT-NAME:UNLOCK-KEY-HEX ...
+       4. reencrypt --resume     ACTIVE-NAME UNLOCK-KEY-HEX
 
 Reencrypt an active encrypted container.
 
 Options:
-    --status      Only query the reencryption status
-    --initialize  Only initialize reencryption
-    --resume      Only resume reencryption
+  --status      Only query the reencryption status
+  --initialize  Only initialize reencryption
+  --resume      Only resume reencryption
+
+For initializing reencryption, all unlock keys must be provided with
+the name of their keyslot.
+
+Arguments:
+  ACTIVE-NAME     Name of the active volume as shown in /dev/mapper
+  KEYSLOT-NAME    Name of the keyslot, as managed by secboot
+  UNLOCK-KEY-HEX  Unlock key (hexadecimal)
 
 Examples:
-    cryptsetup open /dev/vda5 crypt01
-    reencrypt --status crypt01
-    reencrypt --initialize crypt01
-    reencrypt --resume crypt01
+  cryptsetup open /dev/vda5 crypt01
+  reencrypt --status crypt01
+  reencrypt --initialize crypt01 default-recovery:010203... default:00112233...
+  reencrypt --resume crypt01 010203...
 `
 
 func main() {
@@ -31,53 +46,66 @@ func main() {
 		os.Exit(1)
 	}
 
+	log.SetLogLevel(log.LogLevelInfo)
+
 	args = args[1:] // pop argument zero
 	arg := &args[0]
-	args = args[1:] // pop argument zero
-	subcommand := reencrypt
+	var err error
 	switch *arg {
 	case "--status":
-		subcommand = status
-		arg = nil
+		err = status(args[1:]...)
 	case "--initialize":
-		subcommand = initialize
-		arg = nil
+		err = initialize(args[1:]...)
 	case "--resume":
-		subcommand = resume
-		arg = nil
+		err = resume(args[1:]...)
 	default:
-	}
-	if arg == nil {
-		// get it from the next CLI argument
-		if len(os.Args) <= 0 {
-			fmt.Fprintf(os.Stderr, "Missing DISK argument")
-			os.Exit(1)
-		}
-		arg = &args[0]
+		err = reencrypt(args...)
 	}
 
-	err := subcommand(*arg)
 	if err != nil {
 		fmt.Println("error:", err)
 		os.Exit(1)
 	}
 }
 
-func reencrypt(activeName string) error {
+// Split "KEY:VALUE" into "KEY" and "VALUE"
+func splitUnlockKeySpec(s string) (string, string, error) {
+	items := strings.Split(s, ":")
+	if len(items) != 2 {
+		return "", "", fmt.Errorf("invalid format KEY:VALUE")
+	}
+	return items[0], items[1], nil
+}
+
+//func reencrypt(activeName string, unlockKeysHex []string) error {
+func reencrypt(args ...string) error {
+
+	if len(args) < 2 {
+		return fmt.Errorf("reencrypt: not enough arguments")
+	}
+	activeName := args[0]
+	unlockKeysHex := args[1:] // remaining arguments
+
+	log.Info("reencrypt %v with %v key(s)", activeName, len(unlockKeysHex))
 
 	err := status(activeName)
 	if err != nil {
-		fmt.Printf("Cannot get status of %v: %v\n", activeName, err)
 		return err
 	}
 
-	err = initialize(activeName)
+	err = initialize(append([]string{activeName}, unlockKeysHex...)...)
 	if err != nil {
 		fmt.Printf("Cannot initialize reencryption of %v: %v\n", activeName, err)
 		return err
 	}
 
-	err = resume(activeName)
+	// Resume using the first key of the list
+	_, unlockKey, err := splitUnlockKeySpec(unlockKeysHex[0])
+	if err != nil {
+		// Should not happen, as already validated in initialize above
+		return err
+	}
+	err = resume(activeName, unlockKey)
 	if err != nil {
 		fmt.Printf("Cannot resume reencryption of %v: %v\n", activeName, err)
 		return err
@@ -85,46 +113,86 @@ func reencrypt(activeName string) error {
 	return nil
 }
 
-func status(activeName string) error {
+func status(args ...string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("status: bad argument count")
+	}
+	activeName := args[0]
+
+	log.Info("reencrypt status %v", activeName)
+
 	reencryption, err := secboot.ReencryptionForActiveVolume(activeName)
 	if err != nil {
-		fmt.Printf("Cannot find active volume: %v\n", err)
 		return err
 	}
 
 	status, err := reencryption.Status()
 	if err != nil {
-		return fmt.Errorf("Cannot get reencryption status: %w", err)
+		return err
 	}
 	fmt.Println("Status:", status)
 	return nil
 }
 
-func initialize(activeName string) error {
+func initialize(args ...string) error {
+	if len(args) < 2  {
+		return fmt.Errorf("initialize: missing argument")
+	}
+
+	activeName := args[0]
+	unlockKeysHex := args[1:] // remaining arguments
+
+	log.Info("reencrypt initialize %v", activeName)
+
 	reencryption, err := secboot.ReencryptionForActiveVolume(activeName)
 	if err != nil {
-		fmt.Printf("Cannot find active volume: %v\n", err)
-		return err
+		return fmt.Errorf("Cannot find active volume: %w\n", err)
 	}
 
 	unlockKeys := make(map[string][]byte)
-	unlockKeys["1"] = []byte("0000")
+	for _, unlockKeySpec := range unlockKeysHex {
+		// split name:hex into a map
+		key, unlockKeyHex, err := splitUnlockKeySpec(unlockKeySpec)
+		if err != nil {
+			return err
+		}
+
+		unlockKey, err := hex.DecodeString(unlockKeyHex)
+		if err != nil {
+			return fmt.Errorf("Malformed hex '%v': %w", unlockKeyHex, err)
+		}
+		unlockKeys[key] = unlockKey
+	}
+
 	err = reencryption.Initialize(context.Background(), unlockKeys)
 	if err != nil {
-		return fmt.Errorf("cannot initialize: %w", err)
+		return fmt.Errorf("Cannot initialize: %w", err)
 	}
 	fmt.Println("Initialize: ok")
 	return nil
 }
 
-func resume(activeName string) error {
+func resume(args ...string) error {
+	if len(args) != 2  {
+		return fmt.Errorf("initialize: bad argument count")
+	}
+
+	activeName := args[0]
+	unlockKeyHex := args[1]
+
+	log.Info("reencrypt resume %v", activeName)
+	unlockKey, err := hex.DecodeString(unlockKeyHex)
+	if err != nil {
+		return fmt.Errorf("Malformed hex '%v': %w", unlockKeyHex, err)
+	}
+
 	reencryption, err := secboot.ReencryptionForActiveVolume(activeName)
 	if err != nil {
 		fmt.Printf("Cannot find active volume: %v\n", err)
 		return err
 	}
 
-	reencProgressChannel, err := reencryption.Resume(context.Background(), []byte("0000"))
+	reencProgressChannel, err := reencryption.Resume(context.Background(), unlockKey)
 	if err != nil {
 		return fmt.Errorf("cannot resume: %w", err)
 	}
