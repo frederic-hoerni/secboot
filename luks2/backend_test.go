920e282cc41068399dbe0f40f21d4e9dabaac0b0
Frederic Hoerni <frederic.hoerni@canonical.com>
2026-09-18 16:53:33 +0200

    luks2: test NewOnlineReencryption backend

--- luks2/backend_test.go
+++ luks2/backend_test.go
@@ -278,3 +278,35 @@ func (s *backendSuite) TestBackendProbeActivatedNotLUKS2(c *C) {
 
 	c.Check(s.probeCtxs, DeepEquals, []context.Context{ctx})
 }
+
+func (s *backendSuite) TestBackendNewOnlineReencryption(c *C) {
+	// Test NewOnlineReencryption on nominal case
+	mockCryptsetup := snapd_testutil.MockCommand(c, "cryptsetup", "echo device: /dev/sda1; echo reencryption: yes")
+	defer mockCryptsetup.Restore()
+
+	reencryption, err := s.backend.NewOnlineReencryption("some-active-name")
+	c.Assert(err, IsNil)
+	c.Assert(reencryption, NotNil)
+}
+
+func (s *backendSuite) TestBackendNewOnlineReencryptionErrNoDevice(c *C) {
+	// Test NewOnlineReencryption when cryptsetup does not return the underlying disk device
+	mockCryptsetup := snapd_testutil.MockCommand(c, "cryptsetup", "echo reencryption: yes")
+	defer mockCryptsetup.Restore()
+
+	reencryption, err := s.backend.NewOnlineReencryption("some-active-name")
+	c.Assert(err, NotNil)
+	c.Check(err, ErrorMatches, "cannot get device of active name 'some-active-name'")
+	c.Assert(reencryption, IsNil)
+}
+
+func (s *backendSuite) TestBackendNewOnlineReencryptionErr(c *C) {
+	// Test NewOnlineReencryption when cryptsetup exits with 1
+	mockCryptsetup := snapd_testutil.MockCommand(c, "cryptsetup", "exit 1")
+	defer mockCryptsetup.Restore()
+
+	reencryption, err := s.backend.NewOnlineReencryption("some-active-name")
+	c.Assert(err, NotNil)
+	c.Check(err, ErrorMatches, ".*cryptsetup failed with: exit status 1")
+	c.Assert(reencryption, IsNil)
+}
